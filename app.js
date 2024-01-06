ab146c61163d96070ef3ddd7e0eeb29520551866
Mauricio Abregú <mauricio.matias93@gmail.com>
2024-01-06 20:54:01 -0300

    Changes to deploy in Render

--- app.js
+++ app.js
@@ -7,33 +7,33 @@ app.use(express.urlencoded({ extended: true }));
 const products = new ProductManager();
 
 app.get("/products", async (req, res) => {
-  try {
-    const limit = parseInt(req.query.limit);
-    const readProducts = await products.getProducts();
+    try {
+        const limit = parseInt(req.query.limit);
+        const readProducts = await products.getProducts();
 
-    if (!limit) {
-      res.send(readProducts);
-    } else {
-      let productsLimit = readProducts.slice(0, limit);
-      res.send(productsLimit);
+        if (!limit) {
+            res.send(readProducts);
+        } else {
+            let productsLimit = readProducts.slice(0, limit);
+            res.send(productsLimit);
+        }
+    } catch (error) {
+        console.error(`Error while processing request: ${error}`);
+        res.status(500).send("Internal Server Error");
     }
-  } catch (error) {
-    console.error(`Error while processing request: ${error}`);
-    res.status(500).send("Internal Server Error");
-  }
 });
 
 app.get("/products/:pid", async (req, res) => {
-  let product = await products.getProductById(parseInt(req.params.pid));
-  if (!product) {
-    res.send({ error: "Product not found" });
-  } else {
-    res.send(product);
-  }
+    let product = await products.getProductById(parseInt(req.params.pid));
+    if (!product) {
+        res.send({ error: "Product not found" });
+    } else {
+        res.send(product);
+    }
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
-  console.log(`Server Listening in port: ${server.address().port}`);
+    console.log(`Server Listening in port: ${server.address().port}`);
 });
 server.on("error", (error) => console.log(`Server error: ${error}`));
